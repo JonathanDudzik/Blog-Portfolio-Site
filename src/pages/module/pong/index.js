dfaeeb6bcfb883016563a97bc20f9af4b26a0687
JonathanDudzik <jonathan.d.wesley@gmail.com>
2024-07-09 11:26:36 -0400

    commit before pushing to remote

--- src/pages/module/pong/index.js
+++ src/pages/module/pong/index.js
@@ -14,8 +14,12 @@ const Module = () => {
     <div className={styles.moduleToolbar}>
       <Toolbar />
     </div>
-    <div className={styles.moduleMain}>  
-      <iframe src="http://localhost:8001/module/pong" title="pong learning module" width="98%" height="98%" style={{border: "none"}}></iframe>
+    <div className={styles.moduleMain}>
+      <div>
+        <button>Download PDF transcript</button>
+        <a href="google">Download PDF transcript</a>
+        <video>blank</video>
+      </div>
     </div>
   </div>
   )
